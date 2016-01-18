ada71c2b2045bc7098a8c30899dba433c4d0e407
padmanabhan-developer <padmanabhan.developer.email@gmail.com>
2016-01-18 20:04:18 +0530

    Change in db name - nr <-> nw

--- sites/all/themes/cua/panels/layouts/cua_oprettur/test4.php
+++ sites/all/themes/cua/panels/layouts/cua_oprettur/test4.php
@@ -5,7 +5,7 @@ if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 $num = 1;
-$sql2 = "SELECT nw_node.title AS node_title, nw_node.nid AS nid, nw_field_data_field_cykel_aktiv.field_aktiv_value FROM nw_node LEFT JOIN nw_field_data_field_cykel_aktiv ON nw_node.nid = nw_field_data_field_cykel_aktiv.entity_id AND nw_field_data_field_cykel_aktiv.field_aktiv_value = '0' LEFT JOIN  nw_field_data_field_testlokation ON nw_node.nid = nw_field_data_field_testlokation.entity_id AND (nw_field_data_field_testlokation.entity_type = 'node' AND nw_field_data_field_testlokation.deleted = '0') WHERE (( (nw_field_data_field_testlokation.field_testlokation_target_id = '" . $id . "' ) )AND(( (nw_node.status = '1') AND (nw_node.type IN  ('beboer'))  )))";
+$sql2 = "SELECT nr_node.title AS node_title, nr_node.nid AS nid, nr_field_data_field_cykel_aktiv.field_aktiv_value FROM nr_node LEFT JOIN nr_field_data_field_cykel_aktiv ON nr_node.nid = nr_field_data_field_cykel_aktiv.entity_id AND nr_field_data_field_cykel_aktiv.field_aktiv_value = '0' LEFT JOIN  nr_field_data_field_testlokation ON nr_node.nid = nr_field_data_field_testlokation.entity_id AND (nr_field_data_field_testlokation.entity_type = 'node' AND nr_field_data_field_testlokation.deleted = '0') WHERE (( (nr_field_data_field_testlokation.field_testlokation_target_id = '" . $id . "' ) )AND(( (nr_node.status = '1') AND (nr_node.type IN  ('beboer'))  )))";
 // " . $id . "
 $result2 = mysqli_query($con,$sql2);
 /*while($row2 = mysqli_fetch_array($result2)) {
